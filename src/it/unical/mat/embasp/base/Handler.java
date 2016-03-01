d510eaba69e32c753987eeca767529992bdd2f33
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-03-01 13:17:44 +0100

    Added Handler class following the new scheme

---
+++ src/it/unical/mat/embasp/base/Handler.java
@@ -0,0 +1,77 @@
+package it.unical.mat.embasp.base;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Handler {
+
+	private Map<Integer, InputProgram> programs;
+	private Map<Integer, OptionDescriptor> options;
+
+	public Handler() {
+		programs = new HashMap<>();
+		options = new HashMap<>();
+	}
+
+	public int addProgram(InputProgram p){
+
+		int last_index = programs.size();
+		programs.put(last_index++, p);
+		return last_index;
+	}
+
+	public int addOption(OptionDescriptor o){
+		int last_index = options.size();
+		options.put(last_index++, o);
+		return last_index;
+	}
+
+	public void removeProgram(int program_id){
+		programs.remove(program_id);
+	}
+
+	public int removeProgram(InputProgram p){
+		int key = -1;
+		for( Map.Entry<Integer, InputProgram> entry : programs.entrySet()){
+			if(entry.getValue().equals(p)){
+				key = entry.getKey();
+				break;
+			}
+		}
+		if(key != -1){
+			programs.remove(key);
+
+		}
+		return key;
+	}
+
+	public void removeOption(int option_id){
+		options.remove(option_id);
+	}
+
+	public int removeOption(OptionDescriptor o){
+		int key = -1;
+		for( Map.Entry<Integer, OptionDescriptor> entry : options.entrySet()){
+			if(entry.getValue().equals(o)){
+				key = entry.getKey();
+				break;
+			}
+		}
+		if(key != -1){
+			programs.remove(key);
+
+		}
+		return key;
+	}
+
+	public void removeAll(){
+		programs.clear();
+		options.clear();
+	}
+
+	public void startSync(){} // TODO
+
+
+	public void startAsync(){} // TODO
+}
+
