a82d6e18c75a6c5e938400d9df7533043f8ef44b
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-03-01 13:40:06 +0100

    Added Service class , following the new scheme

---
+++ src/it/unical/mat/embasp/base/Service.java
@@ -0,0 +1,22 @@
+package it.unical.mat.embasp.base;
+
+public abstract class Service{
+
+	public Output startSync(){
+		//TODO
+	return null;
+	}
+	
+	public void startAsync(Callback c , InputProgram p , OptionDescriptor o){
+	
+		/*
+		 * 
+		 * Do operation
+		 * 
+		 * 
+		 * c.callback(o); 
+		 */
+			
+	}
+	
+}
