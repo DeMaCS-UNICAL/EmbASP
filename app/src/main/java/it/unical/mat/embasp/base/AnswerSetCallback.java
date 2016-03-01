958338759c848730ba5e6fcdff9f0345f1ac3c51
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-03-01 15:08:58 +0100

    changed project structure

--- app/src/main/java/it/unical/mat/embasp/base/AnswerSetCallback.java
+++
@@ -1,8 +0,0 @@
-package it.unical.mat.embasp.base;
-
-/**
- * <p>AnswerSetCallback an Interface of a callback, called when the result is available</p>
- */
-public interface AnswerSetCallback {
-    public void callback(AnswerSets answerSets);
-}
