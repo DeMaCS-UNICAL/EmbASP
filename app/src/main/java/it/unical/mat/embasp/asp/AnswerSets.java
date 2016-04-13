bceafb1fa228ef69c2760793269a542587c0e9ff
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-04-13 17:03:48 +0200

    Changed AnswerSets structure

--- app/src/main/java/it/unical/mat/embasp/asp/AnswerSets.java
+++ app/src/main/java/it/unical/mat/embasp/asp/AnswerSets.java
@@ -3,21 +3,22 @@ package it.unical.mat.embasp.asp;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-// TODO extend OUTPUT
-public abstract class AnswerSets {
 
-	protected String output;
+import it.unical.mat.embasp.base.Output;
+
+public abstract class AnswerSets extends Output {
+
 	protected List<AnswerSet> answersets;
 
 	public AnswerSets(String answersets) {
 		this.answersets = new ArrayList<>();
-		output = answersets;
+		this.output = answersets;
 		parse();
 	}
 
 	abstract protected void parse();
 
-	public String getAnswerSetsString()  { return output;}
+	public String getAnswerSetsString()  { return this.output;}
 
 	public List<AnswerSet> getAnswersets(){
 		return Collections.unmodifiableList(answersets);
