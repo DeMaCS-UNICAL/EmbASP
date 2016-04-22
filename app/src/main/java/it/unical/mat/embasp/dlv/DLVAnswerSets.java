b4fcd4a4d18787dc3e592627261e828ba585aa54
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-04-22 17:46:26 +0200

    Changed DLVAnswerSets structure

--- app/src/main/java/it/unical/mat/embasp/dlv/DLVAnswerSets.java
+++ app/src/main/java/it/unical/mat/embasp/dlv/DLVAnswerSets.java
@@ -6,6 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import it.unical.mat.embasp.asp.AnswerSets;
+import it.unical.mat.embasp.asp.AnswerSet;
 
 
 public class DLVAnswerSets extends AnswerSets {
@@ -21,7 +22,7 @@ public class DLVAnswerSets extends AnswerSets {
     @Override
     protected void parse() {
         Pattern pattern = Pattern.compile("\\{(.*)\\}");
-        Matcher matcher = pattern.matcher(answerSetsString);
+        Matcher matcher = pattern.matcher(output);
         while (matcher.find()) {
             String answerSet=matcher.group();
             Pattern patternAnswerSet = Pattern.compile("(-?[a-z][A-Za-z0-9_]*(\\(.*?\\))?)(, |\\})");
@@ -30,7 +31,7 @@ public class DLVAnswerSets extends AnswerSets {
             while (matcherAnswerSet.find()) {
                 answerSetList.add(matcherAnswerSet.group(1));
             }
-            answerSetsList.add(new AnswerSet(answerSetList));
+            answersets.add(new AnswerSet(answerSetList));
         }
 
     }
