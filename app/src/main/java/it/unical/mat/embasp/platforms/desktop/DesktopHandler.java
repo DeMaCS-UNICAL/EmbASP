51686d3395012ea88d9a26991d39ce84bba17638
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-05-05 11:14:00 +0200

    changed body structure of startSync and startAsync

--- app/src/main/java/it/unical/mat/embasp/platforms/desktop/DesktopHandler.java
+++ app/src/main/java/it/unical/mat/embasp/platforms/desktop/DesktopHandler.java
@@ -3,7 +3,6 @@ package it.unical.mat.embasp.platforms.desktop;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import it.unical.mat.embasp.base.Callback;
 import it.unical.mat.embasp.base.Handler;
 import it.unical.mat.embasp.base.InputProgram;
@@ -16,83 +15,30 @@ public class DesktopHandler extends Handler{
 	public DesktopHandler(DesktopService service) {
 		this.service = service;
 	}
-	
+
 	@Override
 	public void startAsync(Callback c, List<Integer> program_index,
-			List<Integer> option_index) {
-		
+						   List<Integer> option_index) {
 
-		List<InputProgram> input_programs = new ArrayList<InputProgram>();
-		
-		        if(program_index.isEmpty() || program_index == null) {
-		
-		            for (Map.Entry<Integer, InputProgram> program : this.programs.entrySet()) {
-		                input_programs.add(this.programs.get(program.getKey()));
-		            }
-		
-		        }else{
-		            for(int index : program_index){
-		                input_programs.add(this.programs.get(index));
-		
-		            }
-		        }
-		
-		        List<OptionDescriptor> input_option = new ArrayList<OptionDescriptor>();
-		
-		        if(option_index.isEmpty()|| option_index == null){
-		
-		            for (Map.Entry<Integer, OptionDescriptor> option: this.options.entrySet()) {
-		                input_option.add(this.options.get(option.getKey()));
-		            }
-		
-		        }else{
-		
-		            for(int index : option_index ){
-		                input_option.add(this.options.get(index));
-		            }
-		
-		        }
-		       
-		        service.startAsync(c, input_programs, input_option);
-		        
+
+		List<InputProgram>input_programs = collect_programs();
+		List<OptionDescriptor> input_options = collect_options();
+
+
+
+		service.startAsync(c, input_programs, input_option);
+
 	}
-	
-	
+
+
 	@Override
 	public Output startSync(List<Integer> program_index,
-			List<Integer> option_index) {
-		
-		List<InputProgram> input_programs = new ArrayList<InputProgram>();
-		
-        if(program_index.isEmpty() || program_index == null) {
+							List<Integer> option_index) {
 
-            for (Map.Entry<Integer, InputProgram> program : this.programs.entrySet()) {
-                input_programs.add(this.programs.get(program.getKey()));
-            }
 
-        }else{
-            for(int index : program_index){
-                input_programs.add(this.programs.get(index));
+		List<InputProgram>input_programs = collect_programs();
+		List<OptionDescriptor> input_options = collect_options();
 
-            }
-        }
-
-        List<OptionDescriptor> input_option = new ArrayList<OptionDescriptor>();
-
-        if(option_index.isEmpty()|| option_index == null){
-
-            for (Map.Entry<Integer, OptionDescriptor> option: this.options.entrySet()) {
-                input_option.add(this.options.get(option.getKey()));
-            }
-
-        }else{
-
-            for(int index : option_index ){
-                input_option.add(this.options.get(index));
-            }
-
-        }
-		
-      return service.startSync(input_programs, input_option);
+		return service.startSync(input_programs, input_option);
 	}
 }
