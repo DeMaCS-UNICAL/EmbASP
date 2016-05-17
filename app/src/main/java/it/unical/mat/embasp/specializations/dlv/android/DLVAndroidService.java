f7107ccb3a31f5bc43c4a70bc51d8d06daa95b1e
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-05-17 16:11:04 +0200

    ChangedAndroidService Class now extends BroadCast Reciver

--- app/src/main/java/it/unical/mat/embasp/specializations/dlv/android/DLVAndroidService.java
+++ app/src/main/java/it/unical/mat/embasp/specializations/dlv/android/DLVAndroidService.java
@@ -1,28 +1,27 @@
 package it.unical.mat.embasp.specializations.dlv.android;
 
 import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
-
 import java.util.List;
 import it.unical.mat.embasp.base.Output;
 import it.unical.mat.embasp.base.Callback;
 import it.unical.mat.embasp.base.InputProgram;
 import it.unical.mat.embasp.base.OptionDescriptor;
 import it.unical.mat.embasp.base.Service;
-import it.unical.mat.embasp.platforms.android.AndroidReciver;
 import it.unical.mat.embasp.specializations.dlv.DLVAnswerSets;
 
 //TODO Extend broadCast
-public class DLVAndroidService extends AndroidReciver implements Service {
+public class DLVAndroidService extends BroadcastReceiver implements Service {
 
     private Callback asCallback;
     private Context c;
+
     public DLVAndroidService(Callback callback,Context c) {
-        super(new DLVServiceReasoner("solver"));
         asCallback = callback;
         this.c = c;
     }
@@ -49,9 +48,34 @@ public class DLVAndroidService extends AndroidReciver implements Service {
         stopDlvService(c);
         asCallback = callback;
         Intent intent = new Intent(c, DLVServiceReasoner.class);
+        String input_options = new String();
+
+        input_options+="-silent ";
+
+        for (OptionDescriptor o : options) {
+
+            input_options+= o.getOptions();
+        }
+
+        intent.putExtra(DLVServiceReasoner.OPTION,input_options.toString());
+
+
+        String final_program = new String();
+        String files = new String();
+
+        for (InputProgram p : programs) {
+            final_program += p.getProgram();
+            String program_file = p.getFiles();
+
+            if (program_file != null) {
+                files+= program_file;
+                }
+            }
+
         intent.setAction(DLVServiceReasoner.ACTION_SOLVE);
-        intent.putExtra(DLVServiceReasoner.PROGRAM, programs.toString());
-        intent.putExtra(DLVServiceReasoner.OPTION,options.toString());
+        intent.putExtra(DLVServiceReasoner.PROGRAM, final_program);
+        intent.putExtra(DLVServiceReasoner.FILES, files);
+
         c.registerReceiver(this, new IntentFilter(DLVServiceReasoner.RESULT_NOTIFICATION));
         Log.i(getClass().getName(), " start service");
         c.startService(intent);
