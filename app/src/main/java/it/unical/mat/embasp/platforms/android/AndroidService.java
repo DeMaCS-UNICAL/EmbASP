3f687a7fcac80a2c63ff523877e58c9298be7893
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-05-17 16:10:35 +0200

    Chaned AndroidService class

--- app/src/main/java/it/unical/mat/embasp/platforms/android/AndroidService.java
+++ app/src/main/java/it/unical/mat/embasp/platforms/android/AndroidService.java
@@ -1,13 +1,7 @@
 package it.unical.mat.embasp.platforms.android;
 
 import android.app.IntentService;
-import android.app.Service;
-import android.content.Intent;
-import android.os.Binder;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
 
-//INTENT SERVICE
 public abstract class AndroidService extends IntentService implements it.unical.mat.embasp.base.Service{
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
@@ -17,26 +11,5 @@ public abstract class AndroidService extends IntentService implements it.unical.
     public AndroidService(String name) {
         super(name);
     }
-
-//
-//    // DLVService + ASPSERVice
-//    protected IBinder binder;
-//
-//
-//    /*Returns the current Service class , can be used to interact directly with the Service*/
-//    public class AndroidBinder extends Binder {
-//        public AndroidService getService(){
-//            return AndroidService.this;
-//        }
-//    }
-//
-//    /*used when an activity calls onBind function*/
-//    @Nullable
-//    @Override
-//    public IBinder onBind(Intent intent) {
-//
-//        return binder;
-//    }
-
 
 }
