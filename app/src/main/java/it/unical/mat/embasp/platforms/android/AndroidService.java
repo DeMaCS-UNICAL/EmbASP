ff4c4472604bacabaf0205683cbe55aef424c93a
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-05-17 13:08:30 +0200

    Changed AndroidService to extend IntentService

--- app/src/main/java/it/unical/mat/embasp/platforms/android/AndroidService.java
+++ app/src/main/java/it/unical/mat/embasp/platforms/android/AndroidService.java
@@ -1,31 +1,42 @@
 package it.unical.mat.embasp.platforms.android;
 
+import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
-
-public abstract class AndroidService extends Service implements it.unical.mat.embasp.base.Service{
-
-    protected IBinder binder;
-
-
-    /*Returns the current Service class , can be used to interact directly with the Service*/
-    public class AndroidBinder extends Binder {
-        public AndroidService getService(){
-            return AndroidService.this;
-        }
+//INTENT SERVICE
+public abstract class AndroidService extends IntentService implements it.unical.mat.embasp.base.Service{
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public AndroidService(String name) {
+        super(name);
     }
 
-    /*used when an activity calls onBind function*/
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-
-        return binder;
-    }
+//
+//    // DLVService + ASPSERVice
+//    protected IBinder binder;
+//
+//
+//    /*Returns the current Service class , can be used to interact directly with the Service*/
+//    public class AndroidBinder extends Binder {
+//        public AndroidService getService(){
+//            return AndroidService.this;
+//        }
+//    }
+//
+//    /*used when an activity calls onBind function*/
+//    @Nullable
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//
+//        return binder;
+//    }
 
 
 }
