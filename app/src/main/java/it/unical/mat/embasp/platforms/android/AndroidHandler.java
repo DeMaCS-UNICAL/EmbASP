148e96134da4adfe9c4ad16f093fdeba03f2d567
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-05-18 11:30:29 +0200

    Changed AndroidHandler structure , removed extention to Service

--- app/src/main/java/it/unical/mat/embasp/platforms/android/AndroidHandler.java
+++ app/src/main/java/it/unical/mat/embasp/platforms/android/AndroidHandler.java
@@ -1,32 +1,46 @@
 package it.unical.mat.embasp.platforms.android;
 
 
+import android.content.Context;
+
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import it.unical.mat.embasp.base.Callback;
 import it.unical.mat.embasp.base.Handler;
 import it.unical.mat.embasp.base.InputProgram;
 import it.unical.mat.embasp.base.OptionDescriptor;
-import it.unical.mat.embasp.specializations.dlv.android.DLVAndroidService;
 
 /*This class is a specialization of base.Handler class for Android platform*/
 
 public class AndroidHandler extends Handler {
 
-    /*DLVAndroidService istance , used to start computation*/
-    private DLVAndroidService broadcast;
+    /*AndroidService istance , used to start computation*/
+    private AndroidService service;
 
 
 
-    public AndroidHandler(DLVAndroidService b) {
-        broadcast = b;
+    public AndroidHandler(Context c , Class<?> b) {
+        try {
 
+            service = (AndroidService) ClassLoader.getSystemClassLoader().loadClass(b.getName()).getDeclaredConstructor(Context.class).newInstance(c);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void startAsync(Callback c,List<Integer> program_index, List<Integer> option_index ) {
         List<InputProgram>input_programs = collect_programs(program_index);
         List<OptionDescriptor> input_options = collect_options(option_index);
-        broadcast.startAsync(c, input_programs, input_options);
+        service.startAsync(c, input_programs, input_options);
 
     }
 }
