f96e6268438196e69a420a1553569e01d94b6225
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-05-18 11:31:04 +0200

    Changed AndroidHandler structure : Added Context parameter Added Default Constructor

--- app/src/main/java/it/unical/mat/embasp/platforms/android/AndroidService.java
+++ app/src/main/java/it/unical/mat/embasp/platforms/android/AndroidService.java
@@ -1,15 +1,23 @@
 package it.unical.mat.embasp.platforms.android;
 
-import android.app.IntentService;
 
-public abstract class AndroidService extends IntentService implements it.unical.mat.embasp.base.Service{
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     * @param name Used to name the worker thread, important only for debugging.
-     */
-    public AndroidService(String name) {
-        super(name);
+import android.content.Context;
+
+import java.util.List;
+
+import it.unical.mat.embasp.base.InputProgram;
+import it.unical.mat.embasp.base.OptionDescriptor;
+import it.unical.mat.embasp.base.Output;
+
+public abstract class AndroidService implements it.unical.mat.embasp.base.Service{
+   protected Context context;
+
+    public AndroidService (Context c ){
+    context = c;
     }
 
+    @Override
+    public Output startSync(List<InputProgram> programs, List<OptionDescriptor> options) {
+        return null;
+    }
 }
