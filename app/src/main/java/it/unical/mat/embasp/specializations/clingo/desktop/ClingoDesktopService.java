ee9ba69f6ddcb3ca7348d00a1bf28fec454851e1
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-06-29 17:09:31 +0200

    Generated JavaDoc For ClingoDesktopService class

--- app/src/main/java/it/unical/mat/embasp/specializations/clingo/desktop/ClingoDesktopService.java
+++ app/src/main/java/it/unical/mat/embasp/specializations/clingo/desktop/ClingoDesktopService.java
@@ -10,16 +10,27 @@ import it.unical.mat.embasp.base.Output;
 import it.unical.mat.embasp.platforms.desktop.DesktopService;
 import it.unical.mat.embasp.specializations.clingo.ClingoAnswerSets;
 
+/**is an extention of {@link DesktopService} for Clingo 's solver*/
+
 public class ClingoDesktopService extends DesktopService {
 
-	public ClingoDesktopService(String exe_path) {
-		super(exe_path);
-	}
+    public ClingoDesktopService(String exe_path) {
+        super(exe_path);
+    }
 
+    @Override
+    public Output startSync(List<InputProgram> programs, List<OptionDescriptor> options) {
+
+        return super.startSync(programs, options);
+    }
 
     @Override
     protected AnswerSets getAnswerSet(String output) {
         return new ClingoAnswerSets(output);
     }
 
+    @Override
+    public void startAsync(final Callback callback, final List<InputProgram> programs, final List<OptionDescriptor> options) {
+        super.startAsync(callback, programs, options);
+    }
 }
