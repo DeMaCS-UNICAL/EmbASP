8ab69950cf6c2c83d88d907a52bfd1baf8ec15a9
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-06-29 17:11:14 +0200

    Generated JavaDoc For DLVFilterOption class

--- app/src/main/java/it/unical/mat/embasp/specializations/dlv/DLVFilterOption.java
+++ app/src/main/java/it/unical/mat/embasp/specializations/dlv/DLVFilterOption.java
@@ -1,9 +1,9 @@
 package it.unical.mat.embasp.specializations.dlv;
 
-import it.unical.mat.embasp.base.OptionDescriptor;
+import it.unical.mat.embasp.asp.ASPFilterOption;
 
-public class DLVFilterOption extends OptionDescriptor {
+public class DLVFilterOption extends ASPFilterOption{
     public DLVFilterOption(String initial_option) {
-        super("-filter="+initial_option);
+        super(initial_option);
     }
 }
