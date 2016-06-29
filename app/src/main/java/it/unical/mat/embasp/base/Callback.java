5c5020bb3ee80fd27959dba4545d45fd1f8c3532
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-06-29 16:55:33 +0200

    Generated JavaDoc For Callback class

--- app/src/main/java/it/unical/mat/embasp/base/Callback.java
+++ app/src/main/java/it/unical/mat/embasp/base/Callback.java
@@ -1,6 +1,9 @@
 package it.unical.mat.embasp.base;
 
-/*This interface is used to communicate results from service to the user*/
+/**
+ * This interface is used to communicate results of solver invocation to the user
+ * @see Output
+ */
 public interface Callback {
 	public void callback(Output o);
 }
