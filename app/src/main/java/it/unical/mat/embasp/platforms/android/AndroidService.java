b19b9487bcc240d9a122425a44f3b375628b0dae
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-06-29 16:56:50 +0200

    Generated JavaDoc For AndroidService class

--- app/src/main/java/it/unical/mat/embasp/platforms/android/AndroidService.java
+++ app/src/main/java/it/unical/mat/embasp/platforms/android/AndroidService.java
@@ -2,30 +2,35 @@ package it.unical.mat.embasp.platforms.android;
 
 
 import android.content.Context;
-
 import java.util.List;
-
 import it.unical.mat.embasp.base.Callback;
 import it.unical.mat.embasp.base.InputProgram;
 import it.unical.mat.embasp.base.OptionDescriptor;
 import it.unical.mat.embasp.base.Output;
 
-public class AndroidService implements it.unical.mat.embasp.base.Service{
-   protected Context context;
+/**Implements {@link it.unical.mat.embasp.base.Service} in an Android Context*/
 
-    public AndroidService (Context c ){
-    context = c;
-    }
+public class AndroidService implements it.unical.mat.embasp.base.Service {
+ protected Context context;
 
-    @Override
-    public Output startSync(List<InputProgram> programs, List<OptionDescriptor> options) {
-        return null;
-    }
+ public AndroidService(Context c) {
+  context = c;
+ }
 
-    @Override
-    public void startAsync(Callback callback, List<InputProgram> programs, List<OptionDescriptor> options) {
+ /**Android don't have the capability to perform long tasking operation synchronously
+  * @see #startSync(List, List)
+  * @see android.app.Service*/
+ @Override
+ public Output startSync(List<InputProgram> programs, List<OptionDescriptor> options) {
+  return null;
+ }
 
-    }
+
+ /**@see #startAsync(Callback, List, List)*/
+ @Override
+ public void startAsync(Callback callback, List<InputProgram> programs, List<OptionDescriptor> options) {
+
+ }
 
 
 }
