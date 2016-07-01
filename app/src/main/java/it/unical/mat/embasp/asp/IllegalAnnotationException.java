120f1c7762f8f30c5bf90349f6424082d73c23af
Marco Anastasio <MarcoAnastasio@users.noreply.github.com>
2016-07-01 14:54:17 +0200

    Added IllegalAnnotationException class

---
+++ app/src/main/java/it/unical/mat/embasp/asp/IllegalAnnotationException.java
@@ -0,0 +1,9 @@
+package it.unical.mat.embasp.asp;
+
+
+public class IllegalAnnotationException extends Exception{
+
+    public IllegalAnnotationException(){
+        super("bad annotation");
+    }
+}
