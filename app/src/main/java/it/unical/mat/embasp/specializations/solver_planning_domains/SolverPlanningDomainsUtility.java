68103427ca9351febde94eb60d0060f5c9feb75c
dave90 <davide@davide-All-Series>
2017-03-07 16:27:48 +0100

    Fix import package

--- app/src/main/java/it/unical/mat/embasp/specializations/solver_planning_domains/SolverPlanningDomainsUtility.java
+++ app/src/main/java/it/unical/mat/embasp/specializations/solver_planning_domains/SolverPlanningDomainsUtility.java
@@ -1,7 +1,6 @@
 package it.unical.mat.embasp.specializations.solver_planning_domains;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
