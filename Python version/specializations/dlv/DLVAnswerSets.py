086b803dadba9c70bebe522ac07f634b608beed0
Buonanno <grasta4@gmail.com>
2017-11-01 23:32:41 +0100

    Python DLVParser

--- Python version/specializations/dlv/DLVAnswerSets.py
+++ Python version/specializations/dlv/DLVAnswerSets.py
@@ -1,6 +1,5 @@
 from languages.asp.AnswerSets import AnswerSets
-from languages.asp.AnswerSet import AnserSet
-import re
+from .parser.DLVParserVisitorImplementation import DLVParserVisitorImplementation
 
 class DLVAnswerSets(AnswerSets):
     """Represent an AnswerSet specific for DLV"""
@@ -9,12 +8,4 @@ class DLVAnswerSets(AnswerSets):
         super(DLVAnswerSets, self).__init__(out, err)
         
     def _parse(self):
-        """Create new AnswerSet object represents output come from solver execution and add this in _answersets field"""
-        match = tuple(re.finditer(r"\{(.*)\}", self._output))
-        for m in match:
-            answerSet = m.group()
-            answerSetList = set()
-            matcherAnswerSet = tuple(re.finditer(r"(-?[a-z][A-Za-z0-9_]*(\(.+?\))?)(, |\})", answerSet))
-            for ma in matcherAnswerSet:
-                answerSetList.add(ma.group(1))
-            self._answersets.append(AnserSet(answerSetList))
+        DLVParserVisitorImplementation.parse(self._answersets, self._output)
---
+++ Python version/specializations/dlv/parser/DLVParserVisitorImplementation.py
@@ -0,0 +1,36 @@
+from .DLVLexer import DLVLexer
+from .DLVParser import DLVParser
+from .DLVParserVisitor import DLVParserVisitor
+from antlr4.CommonTokenStream import CommonTokenStream
+from antlr4.InputStream import InputStream
+from languages.asp.AnswerSet import AnserSet
+
+class DLVParserVisitorImplementation(DLVParserVisitor):
+    def __init__(self, answerSets):
+        self._answerSets = answerSets
+        self._cost = None
+    
+    def visitSimpleModel(self, ctx):
+        self._answerSets.append(AnserSet([]))
+        
+        return self.visitChildren(ctx)
+
+    def visitWeightedModel(self, ctx):
+        self._answerSets.append(AnserSet([]))
+        
+        return self.visitChildren(ctx)
+    
+    def visitWitness(self, ctx):
+        self._answerSets.append(AnserSet([]))
+    
+        return self.visitChildren(ctx)
+    
+    def visitCost_level(self, ctx):
+        self._answerSets[-1].getWeights()[ctx.INTEGER_CONSTANT(1).getText()] = ctx.INTEGER_CONSTANT(0).getText()
+        
+    def visitPredicate(self, ctx):
+        self._answerSets[-1].getAnswerSet().append(ctx.getText())
+        
+    @staticmethod
+    def parse(answerSets, dlvOutput):
+        DLVParserVisitorImplementation(answerSets).visit(DLVParser(CommonTokenStream(DLVLexer(InputStream(dlvOutput)))).output())
