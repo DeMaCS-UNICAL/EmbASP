bdb0b6a5563c97063760dc63b366821c32a78eb4
Buonanno <grasta4@gmail.com>
2017-11-01 18:23:09 +0100

    Java SPDParser

--- Java version/app/src/main/java/it/unical/mat/embasp/specializations/solver_planning_domains/SPDPlan.java
+++ Java version/app/src/main/java/it/unical/mat/embasp/specializations/solver_planning_domains/SPDPlan.java
@@ -1,12 +1,7 @@
 package it.unical.mat.embasp.specializations.solver_planning_domains;
 
-import org.json.simple.DeserializationException;
-import org.json.simple.JsonArray;
-import org.json.simple.JsonObject;
-import org.json.simple.Jsoner;
-
-import it.unical.mat.embasp.languages.pddl.Action;
 import it.unical.mat.embasp.languages.pddl.Plan;
+import it.unical.mat.embasp.specializations.solver_planning_domains.parser.SPDGrammarBaseVisitorImplementation;
 
 public class SPDPlan extends Plan {
 
@@ -16,23 +11,10 @@ public class SPDPlan extends Plan {
 
 	@Override
 	protected void parse() {
-		if (errors != "" || output == "")
-			return;
-		JsonObject obj;
-		try {
-			obj = (JsonObject) Jsoner.deserialize(output);
-			final String status = (String) obj.get("status");
-			if (status.contains("ok")) {
-				final JsonArray arrayPlan = (JsonArray) ((JsonObject) obj.get("result")).get("plan");
-				for (int i = 0; i < arrayPlan.size(); i++)
-					actionSequence.add(new Action(((JsonObject) arrayPlan.get(i)).get("name").toString()));
-			} else {
-				final Object resultObj = obj.get("result");
-				errors += resultObj instanceof JsonObject ? " " + (String) ((JsonObject) resultObj).get("error") : resultObj.toString();
-			}
-		} catch (final DeserializationException e) {
-			errors += " ParseException : " + e.getMessage();
-		}
+		if((errors != null && !errors.isEmpty()) || (output != null && output.isEmpty()))
+		    return;
+		
+		errors += SPDGrammarBaseVisitorImplementation.parse(actionSequence, output);
 	}
 
 }
