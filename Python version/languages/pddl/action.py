86c345a92bde8751e302002d734fe561dfc99fe7
SimoneLucia <luciasimone1994@gmail.com>
2018-01-18 16:48:09 +0100

    Fix file naming to python convention

---
+++ Python version/languages/pddl/action.py
@@ -0,0 +1,12 @@
+class Action(object):
+    """Represent a generic Action"""
+    def __init__(self, name):
+        self.__name = name
+        
+    def get_name(self):
+        """Return the name of Action"""
+        return self.__name
+    
+    def set_name(self, name):
+        """Sets the name of Action"""
+        self.name = name
