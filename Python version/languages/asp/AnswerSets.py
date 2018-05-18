99cf190bc49cecc2fcbb5b620c1042f241c5fdc7
Buonanno <grasta4@gmail.com>
2018-05-18 23:34:44 +0200

    OptimalAnswerSets (Python)
    Added method to get only optimal answer sets.

--- Python version/languages/asp/AnswerSets.py
+++ Python version/languages/asp/AnswerSets.py
@@ -1,7 +1,9 @@
 from .AnswerSet import AnswerSet
+from _collections import defaultdict
 from abc import ABCMeta
 from base.Output import Output
 from parsers.asp.ASPDataCollection import ASPDataCollection
+import sys
 
 class AnswerSets(Output, ASPDataCollection):
     """A collection of AnswerSet"""
@@ -18,7 +20,52 @@ class AnswerSets(Output, ASPDataCollection):
             self._answersets = list()
             self._parse()
         return self._answersets
+    
+    def get_optimal_answer_sets(self):
+        answer_sets = self.getAnswerSets()
+        levels = 0
+        minimum_cost_per_level = defaultdict(int)
+        optimal_answer_sets = []
         
+        for answer_set in answer_sets:
+            max_level = max(answer_set.getWeights().keys())
+            
+            if levels < max_level:
+                levels = max_level
+        
+        for level in range(levels, 0, -1):
+            minimum = sys.maxsize
+            
+            for answer_set in answer_sets:
+                not_optimal = False
+                weights = answer_set.getWeights()
+                
+                for previous_level in range(levels, level, -1):
+                    if minimum_cost_per_level[previous_level] != int(weights.get(previous_level, 0)):
+                        not_optimal = True
+                        
+                        break
+                                            
+                if not not_optimal:
+                    current = int(weights.get(level, 0))
+                
+                    if current < minimum:
+                        minimum_cost_per_level[level] = minimum = current 
+
+        for answer_set in answer_sets:
+            not_optimal = False
+            
+            for level in minimum_cost_per_level:
+                if int(answer_set.getWeights().get(level, 0)) != minimum_cost_per_level[level]:
+                    not_optimal = True
+                    
+                    break
+                
+            if not not_optimal:
+                optimal_answer_sets.append(answer_set)
+        
+        return optimal_answer_sets
+    
     def getAnswerSetsString(self):
         """Return a string which contains all AnswerSet"""
         return self._output
