55d5ea385b82c6400d41bbaa590582321e93f001
Buonanno <grasta4@gmail.com>
2018-08-06 12:58:13 +0200

    Removed duplicated files

--- Python version/specializations/dlvhex/DLVHEXAnswerSets.py
+++
@@ -1,9 +0,0 @@
-from languages.asp.AnswerSets import AnswerSets
-from parsers.asp.ASPSolversParser import ASPSolversParser
-
-class DLVHEXAnswerSets(AnswerSets):
-    def __init__(self, output, errors = None):
-        super(DLVHEXAnswerSets, self).__init__(output, errors)
-        
-    def _parse(self):
-        ASPSolversParser.parseDLVHEX(self, self._output, True)
