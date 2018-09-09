7ef9b2e054651a74b7a934c048cae2b009ec1e8f
Buonanno <grasta4@gmail.com>
2018-09-09 22:53:37 +0200

    Renamed files according to python naming conventions
    Note: ANTLR4 auto-generated files have not been modified whatsoever

--- Python version/parsers/asp/asp_solvers_parser.py
+++ Python version/parsers/asp/asp_solvers_parser.py
@@ -1,26 +1,26 @@
-from .clingo.ClingoParserVisitorImplementation import ClingoParserVisitorImplementation
-from .dlv.DLVParserVisitorImplementation import DLVParserVisitorImplementation
-from .dlv2.DLV2ParserVisitorImplementation import DLV2ParserVisitorImplementation
-from .dlvhex.DLVHEXParserVisitorImplementation import DLVHEXParserVisitorImplementation
+from parsers.asp.clingo import clingo_parser_visitor_implementation
+from parsers.asp.dlv import dlv_parser_visitor_implementation
+from parsers.asp.dlv2 import dlv2_parser_visitor_implementation
+from parsers.asp.dlvhex import dlvhex_parser_visitor_implementation
 
 
 class ASPSolversParser(object):
     @staticmethod
     def parse_clingo(answerSets, atomsList, two_stageParsing):
-        ClingoParserVisitorImplementation.parse(
+        clingo_parser_visitor_implementation.parse(
             answerSets, atomsList, two_stageParsing)
 
     @staticmethod
     def parse_dlv(answerSets, atomsList, two_stageParsing):
-        DLVParserVisitorImplementation.parse(
+        dlv_parser_visitor_implementation.parse(
             answerSets, atomsList, two_stageParsing)
 
     @staticmethod
     def parse_dlv2(answerSets, atomsList, two_stageParsing):
-        DLV2ParserVisitorImplementation.parse(
+        dlv2_parser_visitor_implementation.parse(
             answerSets, atomsList, two_stageParsing)
 
     @staticmethod
     def parse_dlvhex(answerSets, atomsList, two_stageParsing):
-        DLVHEXParserVisitorImplementation.parse(
+        dlvhex_parser_visitor_implementation.parse(
             answerSets, atomsList, two_stageParsing)
--- Python version/parsers/pddl/pddl_solvers_parser.py
+++ Python version/parsers/pddl/pddl_solvers_parser.py
@@ -1,8 +1,8 @@
-from .solver_planning_domains.SPDGrammarVisitorImplementation import SPDGrammarVisitorImplementation
+from parsers.pddl.solver_planning_domains import spd_grammar_visitor_implementation
 
 
 class PDDLSolversParser(object):
     @staticmethod
     def parse_solver_planning_domains(actions, spdOutput, two_stageParsing):
-        return SPDGrammarVisitorImplementation.parse(
+        return spd_grammar_visitor_implementation.parse(
             actions, spdOutput, two_stageParsing)
